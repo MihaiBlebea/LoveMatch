ed059facbb3ebe5b907bbb8e61aa2368807d2c77
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-11-11 20:25:23 +0000

    Add interfaces for MessageId and Body Entities

--- src/Domain/Message/Body/BodyInterface.php
+++ src/Domain/Message/Body/BodyInterface.php
@@ -5,5 +5,9 @@ namespace App\Domain\Message\Body;
 
 interface BodyInterface
 {
-    
+    public function __construct(String $body);
+
+    public function getBody();
+
+    public function __toString();
 }
--- src/Domain/Message/MessageId/MessageIdInterface.php
+++ src/Domain/Message/MessageId/MessageIdInterface.php
@@ -5,5 +5,9 @@ namespace App\Domain\Message\MessageId;
 
 interface MessageIdInterface
 {
-    
+    public function __construct($id);
+
+    public function getId();
+
+    public function __toString();
 }
